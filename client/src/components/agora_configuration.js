ed8687b758dfd1ecb6dd5418725feeb86c52a984
William Liu <110699040+waileungl@users.noreply.github.com>
2022-12-14 22:40:37 -0800

    first commit

---
+++ client/src/components/agora_configuration.js
@@ -0,0 +1,15 @@
+import AgoraRTC from 'agora-rtc-sdk-ng';
+import AgoraRTM, { RtmClient } from 'agora-rtm-sdk'
+
+// Initial configuration
+export const APP_ID = "73153c2b05904f79a80ec9ba54084ad5";
+export const TOKEN = null;
+export const videoRoomID = 'video';
+export const dataRoomID = 'data';
+export const codeRoomID = 'code';
+export const config = {
+    mode: 'rtc',
+    codec: 'vp8',
+}
+export const usertcClient = () => { return AgoraRTC.createClient(config) }
+export const usertmClient = () => { return AgoraRTM.createInstance(APP_ID) }
